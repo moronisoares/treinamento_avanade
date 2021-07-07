b69677077e4099f7f15c7237ff13ea5e6d5ea909
Moroni Soares <moroni.soares@aspecti.com.br>
2021-07-07 08:48:24 -0300

    BaseRepository

---
+++ Avanade.SubTCSE.Projeto.Api/Avanade.SubTCSE.Projeto.Data/Repositories/EmployeeRole/EmployeeRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repository;
+using Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base;
+
+namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.EmployeeRole
+{
+    public class EmployeeRoleRepository : BaseRepository<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, string>, IEmployeeRoleRepository
+    {
+
+    }
+}
